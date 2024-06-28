251d9ab0e058635e9447c62e4ae0b12f3103c538
Brad Christie <brad.christie@gmail.com>
2024-06-28 13:26:17 -0400

    Enable gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  basePath: "cigardianangel.com",
+  output: "export",
+  reactStrictMode: true,
+}
 
 export default nextConfig;
