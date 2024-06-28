687098a76b2442cd9cfe859a5ab82ee60a71377e
Brad Christie <brad.christie@gmail.com>
2024-06-28 15:15:30 -0400

    Fix images.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  basePath: process.env.BASE_PATH || "/cigardianangel.com",
+  basePath: process.env.BASE_PATH,
   output: "export",
   reactStrictMode: true,
 }
